5fb07cbf9ee57682302ec8ac66aa3b3d987cf643
wanghaoab <wanghaoab@yonyou.com>
2019-08-30 18:38:37 +0800

    3
    1

---
+++ test.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# a = np.array([1,2,3])
+# b = np.array([[1,2], [3,4]])
+
+# print(a)
+# print(b)
+# print(np.__version__)
+
+a = np.array([1,2,3,4,5], ndmin = 2)
+print(a)
---
+++ test1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# dt = np.dtype(np.int32)
+# print(dt)
+
+# dt = np.dtype('i4')
+# print(dt)
+
+dt = np.dtype([('age', np.int8)])
+print(dt)
+
